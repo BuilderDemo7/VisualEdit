fa1c1d5f9e4d6146dc5bb55b17275781c582264b
BuilderDemo7 <136541955+BuilderDemo7@users.noreply.github.com>
2023-10-31 11:17:34 -0300

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,6 +1,15 @@
 @echo off
+color 9
 echo LuaRT Lua Framework v1.5.2
-echo Building VisualEdit.wlua to VisualEdit.exe ...
-rtc -w VisualEdit.wlua -o bin/VisualEdit.exe
+echo      Building VisualEdit.wlua to bin/VisualEdit.exe ...
+echo #===============================
+echo Creating dynamic:
+rtc -lcanvas.dll -w VisualEdit.wlua -o bin/VisualEdit.exe
+echo @===============================
+echo Creating static:
+rtc -s -lcanvas.dll -w VisualEdit.wlua -o bin/VisualEdit-static.exe
+echo ===============================
 echo Compilation done.
+echo ...
+echo ...
 pause
